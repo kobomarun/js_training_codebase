f71fa8ba65e41191e1373c920237e649ab21c391
Olufemi Oluoje <kobomarun@gmail.com>
2020-04-30 22:41:15 +0100

    modify app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 let name = prompt("What is your name");
 let age = Number(prompt("Hello "+name + " How old are you?")) // expect number value
+
  if(isNaN(age)) {
      alert("Just type number");
      age = Number(prompt("Hello "+name + " How old are you?"))
+     alert("Sorry, we can talk")
   } else {
    alert("Awesome "+name + "your are " +age + "years old" );
  }
